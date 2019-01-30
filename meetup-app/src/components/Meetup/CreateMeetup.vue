ee16429f30a04f016ce0d2f310ca7699f65154ea
Marco Zahn <tulio84z@users.noreply.github.com>
2019-01-30 18:26:50 +0100

    added Create a new meetup screen and changed logic in vuex store

--- meetup-app/src/components/Meetup/CreateMeetup.vue
+++ meetup-app/src/components/Meetup/CreateMeetup.vue
@@ -1,10 +1,102 @@
 <template>
-  <div>
-    <p>The Create Meetup page</p>
-  </div>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6>
+        <h1>Create a new Meetup</h1>
+      </v-flex>
+    </v-layout>
+
+    <v-layout row>
+      <v-flex xs12>
+        <form @submit.prevent="onCreateMeetup">
+          <v-text-field
+            name="title"
+            label="Title"
+            id="title"
+            v-model="title"
+            required
+          >
+          </v-text-field>
+          <v-text-field
+            name="location"
+            label="Location"
+            id="location"
+            v-model="location"
+            required
+          >
+          </v-text-field>
+          <v-textarea
+            name="imageUrl"
+            label="image URL"
+            id="image-url"
+            rows="1"
+            v-model="imageUrl"
+            required
+          >
+          </v-textarea>
+          <v-layout row>
+            <v-flex>
+              <img :src="imageUrl" height="200px">
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex>
+              <v-textarea
+                name="description"
+                label="Description"
+                id="description"
+                v-model="description"
+                required
+              >
+              </v-textarea>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex>
+              <v-btn class="primary" :disabled="!formIsValid" type="submit">
+                Create Meetup
+              </v-btn>
+            </v-flex>
+          </v-layout>
+        </form>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      title: '',
+      location: '',
+      imageUrl: '',
+      description: '',
+    }
+  },
+  computed: {
+    formIsValid() {
+      return this.title !== '' &&
+          this.location !== '' &&
+         this.imageUrl !== '' &&
+         this.description !== ''
+
+    }
+  },
+  methods: {
+    onCreateMeetup: function() {
+
+      const meetupData = {
+        title: this.title,
+        location: this.location,
+        imageUrl: this.imageUrl,
+        description: this.description,
+
+        date: new Date()
+      }
+
+      this.$store.dispatch('createMeetup', meetupData)
+    }
+  }
 }
 </script>
